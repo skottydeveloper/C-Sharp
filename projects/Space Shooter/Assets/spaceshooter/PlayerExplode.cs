755177e2f6db0362d661cb27ce3801b07b36c7c7
Andreas Skotadis <skottydeveloper@gmail.com>
2023-10-01 13:46:42 +1100

    Add space shooter project.

---
+++ projects/Space Shooter/Assets/spaceshooter/PlayerExplode.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// Destroy the player if it collides with a UFO
+/// 
+/// </summary>
+
+public class PlayerExplode : MonoBehaviour {
+
+	public ParticleSystem explosionPrefab;	// an explosion particle effect to play
+	public LayerMask hitLayer = -1;	// the layer that contains UFOs and their bullets
+
+	// Handle collisions
+	// This method is called whenever the object collides with another
+
+	public void OnCollisionEnter2D(Collision2D collision) {
+
+		// check if we collided with a UFO
+		if (IsHit(collision.gameObject)) {
+
+			// if an explosion prefab has been given, 
+			// create an instance at our current location
+
+			if (explosionPrefab != null) {
+				ParticleSystem explosion = Instantiate(explosionPrefab);
+				explosion.transform.position = transform.position;
+			}
+				
+			// Destroy the ship
+			gameObject.SetActive(false);
+		}
+	}
+		
+	// Test if a collision will cause an explosion by checking the layermask
+	private bool IsHit(GameObject gameObject) {
+		return (hitLayer.value & (1 << gameObject.layer)) != 0;
+	}
+
+}
---
+++ projects/Space Shooter/Assets/spaceshooter/PlayerFly.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// Allow the player to fly the sprite around the screen using keyboard or joystick control.
+/// 
+/// </summary>
+
+// requries a Rigidbody2D for physics
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerFly : MonoBehaviour {
+
+	public float speed = 5.0f; // the speed at which the ship moves
+
+	private Rigidbody2D rigidbody; // the ship's rigidbody component
+
+	void Start () {
+		// get the rigidbody component and store it for later use
+		rigidbody = GetComponent<Rigidbody2D>();
+
+		// disable gravity
+		rigidbody.gravityScale = 0;
+
+		// disable rotation
+		rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+	}
+	
+	void Update () {
+
+		// get the controller values from the Input Manager
+		// these are numbers between -1 and 1
+
+		float vx = Input.GetAxis("Horizontal");
+		float vy = Input.GetAxis("Vertical");
+
+		// set the velocity of the ship by scaling the input vector
+		// by the speed value
+
+		rigidbody.velocity = new Vector2(vx, vy) * speed;
+	}
+}
