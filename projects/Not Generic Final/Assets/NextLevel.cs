2382b7a24491c5803449ce963f1bbbde8ebc7757
Andreas Skotadis <skottydeveloper@gmail.com>
2024-08-06 20:49:34 +1000

    Remove redundant projects.

--- projects/Not Generic Final/Assets/NextLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class ExampleClass : MonoBehaviour {
-	void Start () {
-		// Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
-		SceneManager.LoadScene ("Level 1", LoadSceneMode.Additive);
-	}
-}
